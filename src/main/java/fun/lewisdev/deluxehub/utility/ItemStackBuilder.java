e022da5a19916a849570f0e7320ba742d658361f
Tycjan Falana <tycjan0605@gmail.com>
2024-10-27 15:41:49 +0100

    Added the ability to add customModelData to items.

--- src/main/java/fun/lewisdev/deluxehub/utility/ItemStackBuilder.java
+++ src/main/java/fun/lewisdev/deluxehub/utility/ItemStackBuilder.java
@@ -88,6 +88,10 @@ public class ItemStackBuilder {
 			builder.withFlags(flags.toArray(new ItemFlag[0]));
 		}
 
+		if(section.contains("custom_model_data")) {
+			builder.setCustomModelData(section.getInt("custom_model_data"));
+		}
+
 		return builder;
 	}
 
@@ -117,6 +121,13 @@ public class ItemStackBuilder {
 	public ItemStackBuilder withName(String name, Player player) {
 		final ItemMeta meta = ITEM_STACK.getItemMeta();
 		meta.setDisplayName(TextUtil.color(PlaceholderUtil.setPlaceholders(name, player)));
+		ITEM_STACK.setItemMeta(meta);
+		return this;
+	}
+
+	public ItemStackBuilder setCustomModelData(int data) {
+		final ItemMeta meta = ITEM_STACK.getItemMeta();
+		meta.setCustomModelData(data);
 		ITEM_STACK.setItemMeta(meta);
 		return this;
 	}
