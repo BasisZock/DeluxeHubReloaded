80578f37bb00366bcfdc2bca519a920847a20d15
Tycjan Falana <tycjan0605@gmail.com>
2024-11-03 00:46:13 +0100

    ???

--- src/main/java/fun/lewisdev/deluxehub/DeluxeHubPlugin.java
+++ src/main/java/fun/lewisdev/deluxehub/DeluxeHubPlugin.java
@@ -14,6 +14,7 @@ import fun.lewisdev.deluxehub.inventory.InventoryManager;
 import fun.lewisdev.deluxehub.module.ModuleManager;
 import fun.lewisdev.deluxehub.module.ModuleType;
 import fun.lewisdev.deluxehub.module.modules.hologram.HologramManager;
+import fun.lewisdev.deluxehub.module.modules.hotbar.HotbarManager;
 import fun.lewisdev.deluxehub.utility.UpdateChecker;
 import org.bstats.bukkit.MetricsLite;
 import org.bukkit.Bukkit;
