36dedf56f7a8b6a9075efdff0e94cd46372d2872
Strafverfolgung <Khalaf.a@outlook.de>
2025-05-11 13:00:49 +0200

    optimized imports

--- src/main/java/dev/strafbefehl/deluxehubreloaded/config/ConfigMigrator.java
+++ src/main/java/dev/strafbefehl/deluxehubreloaded/config/ConfigMigrator.java
@@ -1,18 +1,13 @@
 package dev.strafbefehl.deluxehubreloaded.config;
 
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 
