f09d2942c7338458e1ee2c58bcafe7375a1ce9c0
BasisZock <kekslecker2000@gmx.de>
2025-06-12 15:54:19 +0200

    Improved PauseAction

--- src/main/java/dev/strafbefehl/deluxehubreloaded/action/ActionManager.java
+++ src/main/java/dev/strafbefehl/deluxehubreloaded/action/ActionManager.java
@@ -46,11 +46,27 @@ public class ActionManager {
 	}
 
 	public void executeActions(Player player, List<String> items) {
-		for (String item : items) {
+		for (String item : items) { // Changed the Lambda to a loop to be able to break out on action
 			String actionName = StringUtils.substringBetween(item, "[", "]");
 			Action action = actionName == null ? null : actions.get(actionName.toUpperCase());
 
 			if ("pause".equalsIgnoreCase(actionName)) {
+				String value = item.contains(" ") ? item.split(" ", 2)[1] : "0";
+				int pause = Integer.parseInt(value);
+
+				// Get remaining actions after this one
+				int currentIndex = items.indexOf(item);
+				List<String> remaining = items.subList(currentIndex + 1, items.size());
+
+				// Join them into a single string, separated by `||`
+				String remainingJoined = String.join("||", remaining);
+
+				// Execute pause and send remaining actions to PauseAction
+				String[] data = { String.valueOf(pause), remainingJoined };
+				String joined = String.join(";", data);
+				action.execute(plugin, player, joined);
+
+				break;
 			} else if (action != null) {
 				item = item.contains(" ") ? item.split(" ", 2)[1] : "";
 				item = PlaceholderUtil.setPlaceholders(item, player);
